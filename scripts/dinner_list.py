9d343f2173f5ea887c02781f7b605e8e0af89d05
ravnkroken <46357680+ravnkroken@users.noreply.github.com>
2019-01-09 11:45:31 +0100

    Cosmetic changes

--- scripts/dinner_list.py
+++ scripts/dinner_list.py
@@ -16,7 +16,7 @@ with open('../middager.md','r') as fp:
 dinners = dinners*54
 
 
-#Make a list of dates, dayofweek and dinner for that day
+#Make a list of dates, dinners and day of week
 first_dinner_day = datetime.datetime(2018,12,30)
 last_dinner_day = first_dinner_day+datetime.timedelta(365-2)
 daynames = calendar.day_name
@@ -25,6 +25,8 @@ dates_with_dinners = rrule(DAILY,dtstart = first_dinner_day,
 dinner_dates = []
 for i,date in enumerate(dates_with_dinners):
     dinner_dates.append([date,calendar.day_name[date.weekday()],dinners[i]])
+    
+#Connect to google calendar API and fill in dinner for each day
 
 
 
